7c8f08caab75035ff431f411da7d569b2428c8c1
PRCHR-u <158329942+PRCHR-u@users.noreply.github.com>
2025-06-19 16:00:23 +0300

    done

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 
@@ -7,7 +8,12 @@ def index(request):
     return HttpResponse("Hello, world! This is the catalog index.")
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    products = Product.objects.all()
+    return render(request, 'catalog/home.html', {'products': products})
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'catalog/product_detail.html', {'product': product})
