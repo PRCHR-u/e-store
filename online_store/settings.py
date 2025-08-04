380693a8fdf4defd3d7626a2358b3c7ba54e541b
PRCHR-u <158329942+PRCHR-u@users.noreply.github.com>
2025-08-04 21:22:51 +0300

    исправления по feature_6

--- online_store/settings.py
+++ online_store/settings.py
@@ -113,6 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
+# Authentication settings
+LOGIN_URL = 'login'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
